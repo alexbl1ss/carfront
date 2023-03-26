23ec07d53d1f3d229b0b6ccf9b054caee2d68cb9
alexbl1ss <alex@brownleeschools.com>
2023-03-26 10:49:38 +0100

    Car edit function added

--- src/components/Carlist.js
+++ src/components/Carlist.js
@@ -3,6 +3,7 @@ import { SERVER_URL } from '../constants.js'
 import { DataGrid } from '@mui/x-data-grid';
 import Snackbar from '@mui/material/Snackbar';
 import AddCar from './AddCar.js';
+import EditCar from './EditCar';
 
 function Carlist() {
   const [cars, setCars] = useState([]);
@@ -53,13 +54,43 @@ function Carlist() {
       })
       .catch(err => console.error(err))
     }
-  
+
+  // Update existing car
+  const updateCar = (car, link) => {
+    fetch(link,
+      { 
+        method: 'PUT', 
+        headers: {
+        'Content-Type':  'application/json',
+      },
+      body: JSON.stringify(car)
+    })
+    .then(response => {
+      if (response.ok) {
+        fetchCars();
+      }
+      else {
+        alert('Something went wrong!');
+      }
+    })
+    .catch(err => console.error(err))
+  }
+
    const columns = [
     {field: 'brand', headerName: 'Brand', width: 200},
     {field: 'model', headerName: 'Model', width: 200},
     {field: 'color', headerName: 'Color', width: 200},
     {field: 'year', headerName: 'Year', width: 150},
     {field: 'price', headerName: 'Price', width: 150},
+    {
+      field: '_links.car.href', 
+      headerName: '', 
+      sortable: false,
+      filterable: false,
+      renderCell: row => <EditCar 
+                            data={row} 
+                            updateCar={updateCar} />
+    },
     {
       field: '_links.self.href', 
       headerName: '', 
